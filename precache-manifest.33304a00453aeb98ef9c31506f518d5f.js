b94f696fddfd02bca73a584021d95caf8319f872
m7mhendawy <m7mhendawy@gmail.com>
2020-05-10 06:52:50 +0200

    Updates

---
+++ precache-manifest.33304a00453aeb98ef9c31506f518d5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ece527d6287bbfeb82e7ba461f69b0",
+    "url": "/M7mdHendawy/Weather/index.html"
+  },
+  {
+    "revision": "e12237f02fa251afad0b",
+    "url": "/M7mdHendawy/Weather/static/css/main.b229cbe7.chunk.css"
+  },
+  {
+    "revision": "91d9e179c2ee50e9d0fb",
+    "url": "/M7mdHendawy/Weather/static/js/2.4c851501.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/M7mdHendawy/Weather/static/js/2.4c851501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e12237f02fa251afad0b",
+    "url": "/M7mdHendawy/Weather/static/js/main.7ddf4feb.chunk.js"
+  },
+  {
+    "revision": "2030e71bd771236ce57e",
+    "url": "/M7mdHendawy/Weather/static/js/runtime-main.aab07b2a.js"
+  }
+]);
